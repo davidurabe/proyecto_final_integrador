c5c03fcb6b4391703e69eda7a54dcda7eb690e35
david burgos <davidezeburgod@gmail.com>
2022-05-12 21:29:43 -0300

    educacion experiencia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { HardandsoftComponent } from './components/hardandsoft/hardandsoft.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtonComponent
+    ButtonComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    HardandsoftComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/hardandsoft/hardandsoft.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HardandsoftComponent } from './hardandsoft.component';
+
+describe('HardandsoftComponent', () => {
+  let component: HardandsoftComponent;
+  let fixture: ComponentFixture<HardandsoftComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HardandsoftComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HardandsoftComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
